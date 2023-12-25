427e2ac2ab6c70197a1dc776b1dc5f33359412ba
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-12-25 22:31:08 +0000

    style nav bar

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type ButtonProps = {
     type: 'button'| 'submit';
     title: string;
@@ -6,12 +8,22 @@ type ButtonProps = {
 }
 const Button = ({ type, title, icon, variant }: ButtonProps) => {
   return (
-      <button
-          type={type}>
-          Login
-      
+    <button
+      className={`flexCenter gap-4 rounded-full border ${variant}`}
+      type={type}
+    >
+      {icon && (
+        <Image
+          src={icon}
+          alt={title}
+          width={24}
+          height={24}
+          className="rounded-full"
+        />
+      )}
+      <label className="bold-16 whitespace-nowrap">{title}</label>
     </button>
-  )
+  );
 }
 
 export default Button
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,145 @@
-import { NAV_LINKS } from "@/constants"
-import Image from "next/image"
-import Link from "next/link"
+// "use client";
+// import { useState } from "react";
+// import { NAV_LINKS } from "@/constants";
+// import Image from "next/image";
+// import Link from "next/link";
+// import Button from "./Button";
+
+// const Navbar = () => {
+//    const [isOpen, setIsOpen] = useState(false);
+
+//    //Handles the opening and closing of our nav
+//    const handleClick = () => {
+//      setIsOpen(!isOpen);
+//    };
+//   return (
+//     <nav className=" flexBetween max-container padding-container relative z-40 py-6">
+//       <Link href="/">
+//         <Image
+//           src="/logo.jpeg"
+//           alt="logo"
+//           width={74}
+//           height={74}
+//           className="rounded"
+//         />
+//       </Link>
+//       <ul className="hidden h-full gap-10 lg:flex ">
+//         {NAV_LINKS.map((link) => (
+//           <Link
+//             key={link.key}
+//             href={link.href}
+//             className="text-gray-90 hover:text-green-50 transition-all hover:font-bold"
+//           >
+//             {link.label}
+//           </Link>
+//         ))}
+//       </ul>
+//       <div className="lg:flexCenter hidden ">
+//         <Button
+//           type="button"
+//           title="Login"
+//           variant="btn_dark_green"
+//           icon="/user.jpeg"
+//         />
+//           </div>
+//           <Link href='/'>
+//               <Image
+//         src="/menu.jpeg"
+//         alt="menu"
+//         width={32}
+//         height={32}
+//         className="inline-block cursor-pointer lg:hidden"
+//         onClick={handleClick}
+//       />
+//           </Link>
+     
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+ "use client";
+import { useState } from "react";
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 import Button from "./Button";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className=" flexBetween max-container padding-container relative z-40 py-6">
+    <nav className=" flexBetween max-container spaceBetween padding-container relative z-40 py-6">
       <Link href="/">
-        <Image src="/logo.jpeg" alt="logo" width={74} height={74} className="rounded" />
+        <Image
+          src="/logo.jpeg"
+          alt="logo"
+          width={74}
+          height={74}
+          className="rounded"
+        />
       </Link>
-      <ul className="hidden h-full gap-10 lg:flex">
-        {NAV_LINKS.map((link) => (
-            <Link key={link.key} href={link.href}
-              className="text-gray-90 hover:text-green-50 transition-all hover:font-bold">{link.label}
-          </Link>
-        ))}
+      <div className="lg:flex hidden items-center">
+        <ul className="hidden h-full gap-12 lg:flex ">
+          {NAV_LINKS.map((link) => (
+            <li key={link.key}>
+              <Link
+                href={link.href}
+                className="text-gray-90 hover:text-green-50 transition-all hover:font-bold"
+              >
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="lg:ml-8">
+          <Button
+            type="button"
+            title="Login"
+            variant="btn_dark_green"
+            icon="/user.jpeg"
+          />
+        </div>
+      </div>
+      <div className="lg:hidden">
+        <div className="relative">
+          <Image
+            src={isOpen ? "/close-icon.jpeg" : "/menu.jpeg"}
+            alt={isOpen ? "close" : "menu"}
+            width={32}
+            height={32}
+            className="inline-block cursor-pointer"
+            onClick={handleClick}
+          />
+          <ul
+            className={`lg:hidden absolute top-16 right-0 bg-white w-40 flex flex-col gap-4 p-4 transform transition-transform duration-300 ease-in-out ${
+              isOpen
+                ? "translate-x-0 opacity-100"
+                : "translate-x-full opacity-0"
+            }`}
+          >
+            {NAV_LINKS.map((link) => (
+              <li key={link.key}>
+                <Link
+                  href={link.href}
+                  className="text-gray-90 hover:text-green-50 transition-all hover:font-bold"
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
           </ul>
-          <div className="lg:flexCenter hidden ">
-              <Button
-                  type="button"
-                  title="Login"
-                  variant="btn_dark_green"
-                  icon="/user.jpeg"
-              />
-              </div>
+        </div>
+      </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
+
+
+
