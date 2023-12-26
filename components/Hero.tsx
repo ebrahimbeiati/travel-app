b738faf9e38ed79e781fbe0649a141790582e5e2
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-12-26 15:33:28 +0000

    finished hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Button from "./Button"
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-yellow-500">
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row ">
       <div className="hero-map" />
       <div className="flex flex-col relative z-20 xl:w-1/2 gap-6">
         <Image
@@ -13,46 +13,66 @@ const Hero = () => {
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
-    
-      <h1 className="bold-64 lg:bold-88 flex">Dream Camp Area</h1>
 
-      <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-        The best place for your next adventure. We can help you on an adventure
-        around the world in just one app.
-      </p>
-      <div className="my-11 flex flex-wrap gap-5">
-        <div className="flex items-center gap-2 ">
-          {Array(5)
-            .fill(1)
-            .map((_, index) => (
+        <h1 className="bold-64 lg:bold-88 flex">Dream Camp Area</h1>
+
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+          The best place for your next adventure. We can help you on an
+          adventure around the world in just one app.
+        </p>
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2 ">
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
+                <Image
+                  key={index}
+                  src="/star.jpeg"
+                  alt="star"
+                  width={20}
+                  height={20}
+                  className="w-5"
+                />
+              ))}
+          </div>
+          <p className="bold-16 lg:bold-20 text-gray-30">
+            100%
+            <span className="regular-16 lg:regular-20 ml-1 text-gray-30">
+              Trusted
+            </span>
+          </p>
+        </div>
+        <div className="flex flex-col w-full gap-5 sm:flex-row">
+          <Button type="button" title="Get Started" variant="btn_green" />
+          <Button type="button" title="Learn More" variant="btn_dark_green" />
+        </div>
+      </div>
+      <div className="relative flex flex-1 items-start ">
+        <div className="relative z-20 w-[260px] flex flex-col gap-8 rounded-3xl bg-gray-90 px-7 py-8">
+          <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20 ">Location</p>
               <Image
-                key={index}
-                src="/star.jpeg"
-                alt="star"
+                src="/location.jpeg"
+                alt="location"
                 width={20}
                 height={20}
-                className="w-5"
+                className="w-5 rounded-full"
               />
-            ))}
-        </div>
-        <p className="bold-16 lg:bold-20 text-gray-30">100%
-        <span className="regular-16 lg:regular-20 ml-1 text-gray-30">
-          Trusted
-          </span>
-          </p>
-      </div>
-      <div className="flex flex-col w-full gap-5 sm:flex-row">
-        <Button
-          type="button"
-          title="Get Started"
-          variant="btn_green"
-          />
-          <Button
-            type="button"
-            title="Learn More"
-            variant="btn_dark_green"
-            />
-      
+            </div>
+            <p className="bold-16 block text-white">Bath</p>
+          </div>
+
+          <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20 ">Distance</p>
+              <p className="bold-16 block text-white ">100mi</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20 ">Elevation</p>
+              <p className="bold-16 block text-white ">3km</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
