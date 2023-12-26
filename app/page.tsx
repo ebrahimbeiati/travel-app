e4def367efca6b0c2dd9ec8727e0dbfd212041ec
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-12-26 13:40:29 +0000

    create different components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-    
+        <Hero />
+        <Camp />
+        <Guide />
+        <Features />
+        <GetApp/>
+        
       </div>
     </main>
   )
